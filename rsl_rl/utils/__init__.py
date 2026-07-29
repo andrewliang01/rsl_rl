5b0e2b48fef29eb8d464643632d009c8d1f568d4
Liang Yujun <yujunliang1016@gmail.com>
2026-07-29 22:23:23 +0800

    部署：新增球面历史策略语义契约

--- rsl_rl/utils/__init__.py
+++ rsl_rl/utils/__init__.py
@@ -7,6 +7,21 @@
 
 from .motion_loader import AMPLoader
 from .motion_loader_for_display import AMPLoaderDisplay
+from .ray_time_deployment_manifest import (
+    RayTimeManifestError,
+    build_ray_time_deployment_manifest,
+    canonical_json_bytes,
+    canonical_json_sha256,
+    collect_git_provenance,
+    default_ray_time_channels,
+    default_ray_time_mount_geometry,
+    default_ray_time_proprio_terms,
+    default_ray_time_tensorization,
+    read_ray_time_deployment_manifest,
+    serialize_ray_time_deployment_manifest,
+    validate_ray_time_deployment_manifest,
+    write_ray_time_deployment_manifest,
+)
 
 from .utils import (
     check_nan,
@@ -21,12 +36,25 @@ from .utils import (
 
 __all__ = [
     "AMPLoader",
+    "RayTimeManifestError",
+    "build_ray_time_deployment_manifest",
+    "canonical_json_bytes",
+    "canonical_json_sha256",
     "check_nan",
+    "collect_git_provenance",
+    "default_ray_time_channels",
+    "default_ray_time_mount_geometry",
+    "default_ray_time_proprio_terms",
+    "default_ray_time_tensorization",
     "get_param",
     "resolve_callable",
     "resolve_nn_activation",
     "resolve_obs_groups",
     "resolve_optimizer",
+    "read_ray_time_deployment_manifest",
+    "serialize_ray_time_deployment_manifest",
     "split_and_pad_trajectories",
     "unpad_trajectories",
+    "validate_ray_time_deployment_manifest",
+    "write_ray_time_deployment_manifest",
 ]
